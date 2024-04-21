77534eaf6c6a382b99aacf1368b835f3a324f114
Alessandro <alessandrofucarino02@gmail.com>
2024-04-21 15:27:58 +0200

    ADD: class and method.

---
+++ src/it/main/StampaMain.java
@@ -0,0 +1,13 @@
+package it.main;
+
+public class StampaMain {
+
+	public static void main(String[] args) {
+				
+		calcolaAreaePerimetro.calcolaArea();
+
+		calcolaAreaePerimetro.calcolaPerimetro();
+		
+
+	}
+}
---
+++ src/it/rettangolo/rettangolo.java
@@ -0,0 +1,13 @@
+package it.rettangolo;
+
+
+
+
+public  class rettangolo { 
+	
+	
+	public int 	base;			
+	public int	altezza;	
+	public int area;
+	public int perimetro;
+}
